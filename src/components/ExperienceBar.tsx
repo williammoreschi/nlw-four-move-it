9f49277c6d2f4d408e42f3d6eb3d90d465e9710e
William Moreschi <williammoreschi@gmail.com>
2021-02-23 21:28:06 -0300

    ADD: A base do primeiro component ExperienceBar

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,13 @@
+export function ExperienceBar(){
+  return(
+    <header className='experience-bar'>
+      <span>0 xp</span>
+      <div>
+        <div style={{width: '60%'}} />
+
+        <span className='current-experience' style={{left:'60%'}} >360xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { ExperienceBar } from "../components/ExperienceBar";
 export default function Home() {
   return (
     <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
