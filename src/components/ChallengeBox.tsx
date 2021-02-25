2f6ac01761757130f40beeb86db1be49739487b8
William Moreschi <williammoreschi@gmail.com>
2021-02-25 00:27:21 -0300

    CHG: Usando o contexto dentro dos componentes

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,21 +1,28 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
-  const hasActiveChallenge = true;
+  const {activeChallenge, resetChallenge} = useContext(ChallengesContext);
+
+  function challengeFailed(){
+    resetChallenge();
+  }
   return(
     <div className={styles.challengeBoxContainer}>
-    {hasActiveChallenge ? (
+    {activeChallenge ? (
       <div className={styles.challengeActive}>
-        <header>Ganhe 400 xp</header>
+        <header>Ganhe {activeChallenge.amount} xp</header>
         <main>
-          <img src="icons/body.svg" alt=""/>
+          <img src={`icons/${activeChallenge.type}.svg`} alt=""/>
           <strong>Exercite-se</strong>
-          <p>É agora Will, bora queimar umas calorias. Faça por 2 minutos alongamento das mãos, para relaxar os músculos.</p>
+          <p>{activeChallenge.description}</p>
         </main>
         <footer>
           <button
           type="button"
           className={styles.challengeFailedButton}
+          onClick={challengeFailed}
           >
             Falhei
           </button>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, ReactNode, useState } from "react";
+import  challenges from '../../challenges.json';
 
+interface IChallenge{
+  type: 'eye' | 'body';
+  description: string;
+  amount: number
+}
 interface IChallengesContextData{
   level: number;
   currentExperience: number;
   challengesCompleted:number;
+  activeChallenge: IChallenge;
+  experienceToNextLevel: number;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface IChallengesProviderProps{
@@ -17,21 +26,35 @@ export function ChallengesProvider({children}:IChallengesProviderProps){
   const [level,setLevel] = useState(1);
   const [currentExperience,setCurrentExperience] = useState(0);
   const [challengesCompleted,setChallengesCompleted] = useState(0);
+  const [activeChallenge,setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4,2);
 
   function levelUp(){
     setLevel(level+1);
   }
 
   function startNewChallenge(){
-    console.log('startNewChallenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
   }
+  
+  function resetChallenge(){
+    setActiveChallenge(null);
+  }
+
   return(
     <ChallengesContext.Provider value={{
       level,
       currentExperience, 
       challengesCompleted,
+      activeChallenge,
+      experienceToNextLevel,
       levelUp,
       startNewChallenge,
+      resetChallenge,
       }}>
       {children}
     </ChallengesContext.Provider>
