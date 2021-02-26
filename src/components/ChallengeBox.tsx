aad1c068a7162c02ef3465b9649d178c5e631dc8
William Moreschi <williammoreschi@gmail.com>
2021-02-25 21:26:58 -0300

    CHG: usando o contexto do CountDown dentro de outro component.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,16 +1,20 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
   const {activeChallenge, resetChallenge, completeChallenge} = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
 
   function handleChallengeCompleted(){
     completeChallenge();
+    resetCountdown();
   }
 
   function handleChallengeFailed(){
     resetChallenge();
+    resetCountdown();
   }
   return(
     <div className={styles.challengeBoxContainer}>
