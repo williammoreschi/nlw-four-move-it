01f7250a35ff955f772baec2702d9868067df56a
William Moreschi <williammoreschi@gmail.com>
2021-03-01 18:56:51 -0300

    CHG: Ajustando o layout ao tamanho da página

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -9,7 +9,7 @@ export function ExperienceBar(){
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{width: `${percentToNextLevel}%`}} />
+        <div style={{width: `${percentToNextLevel}%`}}>&nbsp;</div>
 
         <span className={styles.currentExperience} style={{left:`${percentToNextLevel}%`}} >{currentExperience}xp</span>
       </div>
